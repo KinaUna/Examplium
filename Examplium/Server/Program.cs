452b01558c90b9189a46353f02a5ba523cde02ce
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2022-12-27 06:33:03 +0100

    Moved database connection string to Secrets. Added template for Secrets.

--- Examplium/Server/Program.cs
+++ Examplium/Server/Program.cs
@@ -7,9 +7,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration["DefaultDatabaseConnection"] ?? throw new InvalidOperationException("Configuration string 'DefaultDatabaseConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
