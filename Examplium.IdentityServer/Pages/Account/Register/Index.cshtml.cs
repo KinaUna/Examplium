e2c4f4ca683ca3fb66f291939d2842f66a8a69bd
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2022-12-29 16:25:00 +0100

    Fixed wrong links to register on Login page. Added missing attributes to allow anonymous access to Register and Registered pages.

--- Examplium.IdentityServer/Pages/Account/Register/Index.cshtml.cs
+++ Examplium.IdentityServer/Pages/Account/Register/Index.cshtml.cs
@@ -1,10 +1,13 @@
 using Examplium.Shared.Models.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Examplium.IdentityServer.Pages.Account.Register
 {
+    [AllowAnonymous]
+    [SecurityHeaders]
     public class IndexModel : PageModel
     {
         private readonly UserManager<ApplicationUser> _userManager;
--- Examplium/Shared/Constants/ExampliumAuthServerConstants.cs
+++ Examplium/Shared/Constants/ExampliumAuthServerConstants.cs
@@ -3,7 +3,7 @@
     public static class ExampliumAuthServerConstants
     {
         public const string WebServerUrlDebug = "https://localhost:7128";
-
+        public const string IdentityServerUrlDebug = "https://localhost:7169";
         public const string CoreApiName = "Examplium.CoreApi";
     }
 }
