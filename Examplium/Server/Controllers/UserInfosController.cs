14d7271796cd77e9e567fd3b48661d3afe85f618
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2023-01-23 11:58:17 +0100

    Added UserInfosController to Examplium.Server project. Added default Get method, which returns the current user's UserInfo. Added UpdateCurrentUser post method for updating the current user's UserInfo.

---
+++ Examplium/Server/Controllers/UserInfosController.cs
@@ -0,0 +1,36 @@
+﻿using Examplium.Server.Services.Notes;
+using Examplium.Server.Services.UserInfos;
+using Examplium.Shared.Models.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Examplium.Server.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserInfosController : ControllerBase
+    {
+        private readonly IUserInfosService _userInfosService;
+        public UserInfosController(IUserInfosService userInfosService)
+        {
+            _userInfosService = userInfosService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var currentUserResponse = await _userInfosService.GetCurrentUserInfo();
+
+            return Ok(currentUserResponse);
+        }
+        
+        [HttpPost("UpdateCurrentUser")]
+        public async Task<ActionResult> UpdateCurrentUser(UserInfo userInfo)
+        {
+            var updateCurrentUserResponse = await _userInfosService.UpdateCurrentUserInfo(userInfo);
+
+            return Ok(updateCurrentUserResponse);
+        }
+    }
+}
